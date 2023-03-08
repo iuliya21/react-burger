c0af79e57c62037a858c971d14df9ef6fdb74fbe
iuliya21 <iuliya21@mail.ru>
2023-03-08 19:34:06 +0300

    fix: propTypes in component ingredient

--- src/components/ingredient/ingredient.jsx
+++ src/components/ingredient/ingredient.jsx
@@ -16,19 +16,10 @@ function Ingredient({ name, price, image}) {
   )
 }
 
-Ingredient.propTypes = ({
-  _id: PropTypes.string.isRequired,
+Ingredient.propTypes = {
   name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  proteins: PropTypes.number.isRequired,
-  fat: PropTypes.number.isRequired,
-  carbohydrates: PropTypes.number.isRequired,
-  calories: PropTypes.number.isRequired,
   price: PropTypes.number.isRequired,
   image: PropTypes.string.isRequired,
-  image_mobile: PropTypes.string.isRequired,
-  image_large: PropTypes.string.isRequired,
-  __v: PropTypes.number.isRequired,
-}).isRequired;
+};
 
 export default Ingredient;
