173a2c81448b7198bbcd5edb4b492fc698644789
iuliya21 <iuliya21@mail.ru>
2023-03-30 14:45:54 +0300

    feat store

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { legacy_createStore as createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './components/app/app';
-import reportWebVitals from './reportWebVitals';
+import { rootReducer } from './services/reducers';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers();
+
+const store = createStore(rootReducer, enhancer); // создание хранилища
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
